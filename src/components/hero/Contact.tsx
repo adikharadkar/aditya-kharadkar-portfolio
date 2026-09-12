6292ae46379140bbd1aadb1c0271802e1744ffb2
Aditya Kharadkar <58508431+adikharadkar@users.noreply.github.com>
2026-09-12 19:24:21 +0530

    Update download link for resume file

--- src/components/hero/Contact.tsx
+++ src/components/hero/Contact.tsx
@@ -29,7 +29,7 @@ export default function Contact() {
       >
         <li>
           <a
-            href="/Aditya_Kharadkar_Frontend_Developer_4.7yrs.pdf"
+            href="/Aditya_Kharadkar_Frontend_Developer_5yrs.docx.pdf"
             download
             className="
             inline-flex items-center justify-center
